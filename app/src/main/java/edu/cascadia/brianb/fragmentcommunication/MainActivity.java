95417825e3f1950e274e10ff41ff3ad35f4f3766
Cascadia-PawanjitNayyar <pawanjit.kaur1@gmail.com>
2015-11-09 21:07:03 -0800

    Updated the fragment code

--- app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
@@ -1,10 +1,13 @@
 package edu.cascadia.brianb.fragmentcommunication;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+        implements ItemFragment.OnFragmentInteractionListener{
 //TODO: require the Activity to implement the interface 'OnFragmentInteractionListener'
 
     @Override
@@ -12,8 +15,32 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO: Use the fragment transaction manager to add the item fragment to the fragment container
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new ItemFragment())
+        fragmentTransaction.commit();
     }
 
     //TODO: Implement the method 'onFragmentInteraction' to creates a new SimpleFragment using its FragmentFactory method 'newInstance'
 
+    @Override
+      public void onFragmentInteraction(String id) {
+
+              getFragmentManager().beginTransaction()
+                               .replace(R.id.fragment_container, SimpleFragment.newInstance(id))
+                               .addToBackStack(null)
+                               .commit();
+           }
+
+
+               public void onBackPressed() {
+                   if(getFragmentManager().getBackStackEntryCount() > 0){
+                       getFragmentManager().popBackStack();
+                   }
+                   else {
+                       super.onBackPressed();
+                   }
+    }
 }
