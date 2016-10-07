48b472a1148f953f3304e4f541a67649c96a571b
Benmoony <benmiller92@gmail.com>
2016-10-07 10:22:34 -0700

    In class excercise

--- app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+implements ItemFragment.OnFragmentInteractionListener{
 //TODO: require the Activity to implement the interface 'OnFragmentInteractionListener'
 
     @Override
@@ -12,8 +13,26 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO: Use the fragment transaction manager to add the item fragment to the fragment container
+
+        if (savedInstanceState == null){
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container,ItemFragment.newInstance(),"firstFrag")
+                    .commit();
+        }
+
     }
 
     //TODO: Implement the method 'onFragmentInteraction' to creates a new SimpleFragment using its FragmentFactory method 'newInstance'
 
+    @Override
+    public void onFragmentInteraction(String id) {
+        final SimpleFragment simpleFragment = SimpleFragment.newInstance(id);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, simpleFragment, "SimpleString")
+                .addToBackStack(null)
+                .commit();
+
+    }
 }
