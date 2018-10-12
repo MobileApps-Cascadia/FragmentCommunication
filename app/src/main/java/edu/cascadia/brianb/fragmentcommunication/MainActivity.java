97ca2665d3b77824dfcaca70ffad77658d5a5ae0
Mei'lani <meyrehay@gmail.com>
2018-10-11 19:00:06 -0700

    Implemented the interface and returned the position to the view

--- app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
@@ -4,16 +4,17 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
-//TODO: require the Activity to implement the interface 'OnFragmentInteractionListener'
+public class MainActivity extends Activity implements ItemFragment.OnFragmentInteractionListener{
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO: Add the ItemFragment to the Activity layout's fragment container
-        // HINT: Use the getFragmentManager to begin a transaction, add, and commit
+        getFragmentManager().beginTransaction().add(R.id.fragment_container, new ItemFragment()).commit();
+    }
 
+    @Override
+    public void onFragmentInteraction(String id) {
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container, SimpleFragment.newInstance(id)).addToBackStack("ItemList").commit();
     }
 
     //TODO: Implement the method 'onFragmentInteraction' to creates a new SimpleFragment using its FragmentFactory method 'newInstance'
