32927aa4ec19fa7f96a39b8f2d85246d06775c43
Cascadia-EmilioDiaz <xenorai@gmail.com>
2018-10-12 12:48:52 -0700

    Done
    Everything should be done.

--- app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ItemFragment.OnFragmentInteractionListener {
 //TODO: require the Activity to implement the interface 'OnFragmentInteractionListener'
 
     @Override
@@ -13,9 +13,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         //TODO: Add the ItemFragment to the Activity layout's fragment container
         // HINT: Use the getFragmentManager to begin a transaction, add, and commit
-
+        getFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, new ItemFragment())
+                .commit();
     }
 
     //TODO: Implement the method 'onFragmentInteraction' to creates a new SimpleFragment using its FragmentFactory method 'newInstance'
-
+    public void onFragmentInteraction(String s){
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, SimpleFragment.newInstance(s))
+                .addToBackStack("back")
+                .commit();
+    }
 }
