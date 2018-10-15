03c60a17ae3ac667e228d1806250b9c5c541f327
Juanitaag1 <35508373+Juanitaag1@users.noreply.github.com>
2018-10-15 14:27:59 -0700

    didTheToDos

--- app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
@@ -1,21 +1,38 @@
 package edu.cascadia.brianb.fragmentcommunication;
 
 import android.app.Activity;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
-public class MainActivity extends Activity {
-//TODO: require the Activity to implement the interface 'OnFragmentInteractionListener'
+public class MainActivity extends Activity implements ItemFragment.OnFragmentInteractionListener {
+//: require the Activity to implement the interface 'OnFragmentInteractionListener'
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO: Add the ItemFragment to the Activity layout's fragment container
+        //: Add the ItemFragment to the Activity layout's fragment container
         // HINT: Use the getFragmentManager to begin a transaction, add, and commit
-
+getFragmentManager()
+        .beginTransaction()
+        .add(R.id.fragment_container,new ItemFragment())
+        .commit();
     }
 
     //TODO: Implement the method 'onFragmentInteraction' to creates a new SimpleFragment using its FragmentFactory method 'newInstance'
 
+
+    @Override
+    public void onFragmentInteraction(String id) {
+        SimpleFragment sF = SimpleFragment.newInstance(id);
+
+        getFragmentManager()
+                .beginTransaction()
+               //.add(R.id.resultTextView, sF)
+
+      .replace(R.id.fragment_container,sF)
+                .commit();
+    }
 }
