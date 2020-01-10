9bc6200a34e1c1b89a187533e361635af5dd6ac1
grantlbuchanan <grantsrockets@yahoo.com>
2020-01-09 17:18:10 -0800

    making progress with fragments

--- app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
@@ -1,16 +1,23 @@
 package edu.cascadia.brianb.fragmentcommunication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.view.menu.MenuView;
+import androidx.fragment.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity extends AppCompatActivity  implements ItemFragment.OnFragmentInteractionListener{
 //TODO: require the Activity to implement the interface 'OnFragmentInteractionListener'
-
+    FragmentManager fragmentManager;
+    private ItemFragment.OnFragmentInteractionListener mListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.fragment_container, new ItemFragment(), "frag1")
+                .commit();
         //TODO: Add the ItemFragment to the Activity layout's fragment container
         // HINT: Use the getFragmentManager to begin a transaction, add, and commit
 
@@ -18,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
     //TODO: Implement the method 'onFragmentInteraction' to creates a new SimpleFragment
     //      using its FragmentFactory method 'newInstance'
+
+    public void OnFragmentInteractionListener(String ID){
+        fragmentManager.beginTransaction()
+                .add(R.id.fragment_container, SimpleFragment.newInstance(ID), "frag2")
+                .commit();
+
+    }
+
 
 }
