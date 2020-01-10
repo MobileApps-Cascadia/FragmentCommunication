6ac897476f9c8d40d503b6120a4b917feae9beee
Victoria <tazzygirl37@gmail.com>
2020-01-10 12:56:41 -0800

    Establishes communication between the two fragments, and shows the number selected in fragment 1, on fragment 2

--- app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
+++ app/src/main/java/edu/cascadia/brianb/fragmentcommunication/MainActivity.java
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnFragmentInteractionListener {
 //TODO: require the Activity to implement the interface 'OnFragmentInteractionListener'
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +14,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //TODO: Add the ItemFragment to the Activity layout's fragment container
         // HINT: Use the getFragmentManager to begin a transaction, add, and commit
+        getFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, new ItemFragment())
+                .commit();
 
     }
 
     //TODO: Implement the method 'onFragmentInteraction' to creates a new SimpleFragment
     //      using its FragmentFactory method 'newInstance'
 
+
+    @Override
+    public void onFragmentInteraction(String id) {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new SimpleFragment().newInstance(id))
+                .addToBackStack(null)
+                .commit();
+    }
 }
